bb51d79644299edd93f13e997163f13b4437d4d2
Pei Sun <peis@alluxio.com>
2016-06-14 10:26:41 -0700

    Add a tool to publish messages to a Kafka topic.

---
+++ examples/src/main/java/alluxio/examples/KafkaWordCountProducer.java
@@ -0,0 +1,68 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.examples;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Generate unique words and publish them to a kafka topic.
+ */
+public class KafkaWordCountProducer {
+  public static void main(String[] args) throws Exception {
+    if (args.length < 5) {
+      System.err.println(
+          "Usage: JavaKafkaWordCount <brokerList> <topic> <messagesPerSec> <wordsPerMessage> "
+              + "<maxUniqueMessageCount>");
+      System.exit(1);
+    }
+
+    Logger.getRootLogger().setLevel(Level.ERROR);
+
+    final String brokers = args[0];
+    final String topic = args[1];
+    final int messagesPerSeccond = Integer.parseInt(args[2]);
+    final int wordsPerMessage = Integer.parseInt(args[3]);
+    final long maxUniqueMessageCount = Long.parseLong(args[4]);
+
+    Map<String, Object> configs = new HashMap<>();
+    configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+    configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+    RateLimiter rateLimiter = RateLimiter.create(messagesPerSeccond);
+    long counter = 0;
+    while (true) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < messagesPerSeccond; i++) {
+        sb.append(counter + " ");
+        counter++;
+        if (maxUniqueMessageCount >= 0) {
+          counter %= maxUniqueMessageCount;
+        }
+      }
+      ProducerRecord<String, String> message = new ProducerRecord<>(topic, null, sb.toString());
+      producer.send(message);
+      rateLimiter.acquire();
+    }
+  }
+}

a113ad847fa6ca9165f536c1b2a985cddd8c0e9c
Pei Sun <peis@alluxio.com>
2017-02-19 01:10:00 -0800

    Client side changes to use UFSBlockStore

--- core/server/worker/src/main/java/alluxio/worker/netty/DataServerHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/netty/DataServerHandler.java
@@ -21,6 +21,7 @@ import alluxio.network.protocol.RPCMessage;
 import alluxio.network.protocol.RPCProtoMessage;
 import alluxio.network.protocol.RPCRequest;
 import alluxio.network.protocol.RPCResponse;
+import alluxio.network.protocol.RPCUfsBlockReadRequest;
 import alluxio.worker.AlluxioWorkerService;
 
 import com.google.common.base.Preconditions;
@@ -80,6 +81,10 @@ final class DataServerHandler extends SimpleChannelInboundHandler<RPCMessage> {
       case RPC_FILE_WRITE_REQUEST:
         assert msg instanceof RPCFileWriteRequest;
         mUnderFileSystemHandler.handleFileWriteRequest(ctx, (RPCFileWriteRequest) msg);
+        break;
+      case RPC_UFS_BLOCK_READ_REQUEST:
+        assert msg instanceof RPCUfsBlockReadRequest;
+        mBlockHandler.handleUfsBlockReadRequest(ctx, (RPCUfsBlockReadRequest) msg);
         break;
       case RPC_ERROR_RESPONSE:
         assert msg instanceof RPCErrorResponse;
